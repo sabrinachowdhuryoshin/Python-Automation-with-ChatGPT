85a453aa859a29b6426cbb351496c568b4f56c76
sabrinachowdhuryoshin <sabrina.chowdhury29@gmail.com>
2023-03-10 22:36:59 +0000

    update

--- python-chatgpt.py
+++ python-chatgpt.py
@@ -9,14 +9,20 @@ api_endpoint = "https://api.openai.com/v1/completions"
 # An empty API key for now, will be updated with the actual key
 api_key = ""
 
-# The request headers containing the API key in the authorization header
+# Define the headers that will be sent in the API request
 request_headers = {
-    'Content-Type: application/json',
-    'Authorization: Bearer ' + api_key
+    'Content-Type': 'application/json',  # The content type of the request body
+    'Authorization': 'Bearer ' + api_key  # The authorization token for the API request
 }
 
-# The request data that we will send to the API endpoint
-request_data = {}
+# Define the data that will be sent in the API request
+request_data = {
+    'model': 'text-davinci-003',  # The OpenAI model to use for generating the response
+    'prompt': 'Hello, how are you?',  # The text prompt to generate a response to
+    'max_tokens': 10,  # The maximum number of tokens (words or punctuation marks) to generate in the response
+    'temperature': 0.5  # The level of randomness in the generated response
+}
+
 
 # Sending a HTTP POST request to the API endpoint with the request headers and data
 response = requests.post(api_endpoint, headers = request_headers, json = request_data)
