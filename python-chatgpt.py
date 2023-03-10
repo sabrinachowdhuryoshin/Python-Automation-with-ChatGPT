6ebaacc2017e19f4772bee8f89a0994162d3309e
Sabrina Chowdhury <sabrina.chowdhury_1@nexperia.com>
2023-03-10 21:58:48 +0000

    new module added

---
+++ python-chatgpt.py
@@ -0,0 +1,22 @@
+## Importing the necessary libraries for the script
+import requests # for making HTTP requests to the API
+import os # for accessing the operating system
+import openai # for interfacing with the OpenAI API
+
+## The API endpoint URL that we will use to make requests to the OpenAI API
+api_endpoint = "https://api.openai.com/v1/completions"
+
+# An empty API key for now, will be updated with the actual key
+api_key = ""
+
+# The request headers containing the API key in the authorization header
+request_headers = {
+    'Content-Type: application/json',
+    'Authorization: Bearer ' + api_key
+}
+
+# The request data that we will send to the API endpoint
+request_data = {}
+
+# Sending a HTTP POST request to the API endpoint with the request headers and data
+response = requests.post(api_endpoint, headers = request_headers, json = request_data)
