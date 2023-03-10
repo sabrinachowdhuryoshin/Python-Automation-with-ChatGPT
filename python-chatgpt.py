48f95ce25c5ea2a235333713069dda8691311109
sabrinachowdhuryoshin <sabrina.chowdhury29@gmail.com>
2023-03-10 22:39:58 +0000

    update

--- python-chatgpt.py
+++ python-chatgpt.py
@@ -18,9 +18,9 @@ request_headers = {
 # Define the data that will be sent in the API request
 request_data = {
     'model': 'text-davinci-003',  # The OpenAI model to use for generating the response
-    'prompt': 'Hello, how are you?',  # The text prompt to generate a response to
-    'max_tokens': 10,  # The maximum number of tokens (words or punctuation marks) to generate in the response
-    'temperature': 0.5  # The level of randomness in the generated response
+    'prompt': 'Write python script for hello world',  # The text prompt to generate a response to
+    'max_tokens': 100,  # The maximum number of tokens (words or punctuation marks) to generate in the response
+    'temperature': 0.5  # The level of randomness in the generated response, half creative half predictable
 }
 
 # Sending a HTTP POST request to the API endpoint with the request headers and data
